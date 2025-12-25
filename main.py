fad1d91f2b65064e8f46cadc55e6196d3a5f9420
Sed-Miyuki <Vianviceroy007@gmail.com>
2025-12-25 22:21:17 +0530

    collision

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import pygame
 import constants
 import player
+import asteroids
+import sys
+import asteroid_field
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
+    screen=pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
     clock =pygame.time.Clock()
     dt=0
-    cplay=player.player(constants.SCREEN_WIDTH/2,constants.SCREEN_HEIGHT/2)
-    screen=pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    astra=pygame.sprite.Group()
+    player.player.containers=(updatable, drawable)
+    asteroids.Asteroid.containers=(astra,updatable,drawable)
+    asteroid_field.AsteroidField.containers=(updatable)
+    player_obj=player.player(constants.SCREEN_WIDTH/2,constants.SCREEN_HEIGHT/2)
+    astrof_obj=asteroid_field.AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        cplay.update(dt)
-        cplay.draw(screen)
+        dt=clock.tick(60)/1000
+        updatable.update(dt)
+        for castra in astra:
+            if player_obj.collides_with(castra):
+                print("Game over!")
+                sys.exit()
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        dt=clock.tick(60)
-        dt/=1000
 
 if __name__ == "__main__":
     main()
